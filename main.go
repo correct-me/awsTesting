624ddea8268dc46ecaa6495237a454a2588b3d2b
ivansizov <sizovivan.05@gmail.com>
2025-02-27 00:53:17 +0700

    read/write objects from bucket test

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"bytes"
 	"context"
-	"io/ioutil"
+	"io"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -12,14 +13,9 @@ import (
 )
 
 const bucket = "testbucketcorrectme"
-const key = "example.txt"
+const key = "2025-02-06 17-28-35.mp4"
 
 func main() {
-	data, err := ioutil.ReadFile("example.txt")
-	if err != nil {
-		log.Fatalf("error while reading file: %v", err)
-	}
-
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ru-central1"))
 	if err != nil {
 		log.Fatalf("error while configuring S3: %v", err)
@@ -27,16 +23,61 @@ func main() {
 
 	s3Client := s3.NewFromConfig(cfg)
 
-	result, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+	if err := uploadFileToBucket(s3Client, "media/2025-02-06 17-28-35.mp4"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := downloadFileFromBucket(s3Client, "media/2025-02-06 17-28-35_COPY.mp4"); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func uploadFileToBucket(client *s3.Client, filepath string) error {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	if _, err := client.PutObject(context.TODO(), &s3.PutObjectInput{
 		Bucket:      aws.String(bucket),
 		Key:         aws.String(key),
 		Body:        bytes.NewReader(data),
 		ContentType: aws.String("text/plain"),
-	})
-
-	if err != nil {
-		log.Fatalf("error while sending object to bucket: %v", err)
+	}); err != nil {
+		return err
 	}
 
-	log.Println("object sent successfully, size:", result.Size)
+	log.Println("object sent successfully")
+
+	return nil
+}
+
+func downloadFileFromBucket(client *s3.Client, dir string) error {
+	result, err := client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return err
+	}
+
+	objectsBytes, err := io.ReadAll(result.Body)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(dir)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(objectsBytes)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("successfully downloaded data from %s/%s\n to file %s", bucket, key, dir)
+	return nil
 }
